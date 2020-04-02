75d78a5f5032f9f7c6bfd5618b5d60080c3047b5
rishitaggarwal1 <rishitaggarwal1@gmail.com>
2020-04-02 22:27:59 +0530

    Login Page Backend Completed

--- chat_site/views.py
+++ chat_site/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 
@@ -11,10 +11,27 @@ def login(request):
     from random import randint
     otp = 0
     if request.method == "POST":
-        phone_no = request.POST['phone_no']
-        otp = randint(11111, 99999)
-        print("Rishit")
-        # return render(request, 'login.html', {
-        #     'otp': otp
-        # })
+        if 'next_button' in request.POST and request.POST['phone_no']:
+            otp = randint(11111, 99999)
+            print(otp)
+            return render(request, 'login.html', {
+                'otp': otp
+            })
+        elif 'otp_submit' in request.POST and request.POST['otp']:
+            if request.POST['old_otp'] == request.POST['otp']:
+                message = "Logged In"
+                return render(request, 'login.html', {
+                    'message': message
+                })
+            else:
+                message = "Wrong OTP"
+                otp = randint(11111, 99999)
+                print(otp)
+                return render(request, 'login.html', {
+                    'message': message,
+                    'otp': otp
+                })
+        elif 'submit' in request.POST and request.POST['username']:
+            message = "Logged In as "+request.POST['username']
+            return HttpResponse(message)
     return render(request, 'login.html', {})
